da7959a6443bff42d72f242efe655b66c15b7f20
JudeWillow <lumenos1089@gmail.com>
2026-01-06 19:56:23 +0000

    Enemy animation
    Made a simple looping animation for the mushroom enemy that always runs

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -1,10 +1,18 @@
 extends CharacterBody2D
 
 @export var movementSpeed = 20.0
-
+@onready var sprite = $Sprite2D
+@onready var anim = $Walk
 @onready var player = get_tree().get_first_node_in_group("player") ## Gets the player node so the enemy knows what the player is
+
+func _ready():
+	anim.play("Walk")
 ## onready gets the value after the node has loaded
 func _physics_process(_delta):
 	var direction = global_position.direction_to(player.global_position) ## Compares position of enemy to the player as a vector on a graph
 	velocity = direction*movementSpeed ## Moves towards the player based on the direction
 	move_and_slide() ## Tells Godot the enemy wants to move
+	if direction.x > 0:
+		sprite.flip_h = true
+	elif direction.x < -0:
+		sprite.flip_h = false
--- Player/player.gd
+++ Player/player.gd
@@ -16,13 +16,13 @@ func movement():
 	elif move.x < 0:
 		sprite.flip_h = false
 	
-	if move != Vector2.ZERO:
-		if walkTimer.is_stopped():
-			if sprite.frame >= sprite.hframes - 1:
-				sprite.frame = 0
+	if move != Vector2.ZERO: ## If there is movement
+		if walkTimer.is_stopped(): ## if the timer isnt running
+			if sprite.frame >= sprite.hframes - 1: ## If we have already moved 1 frame of the animation
+				sprite.frame = 0 ## Set the frame back to 0
 			else:
-				sprite.frame += 1
-			walkTimer.start()
+				sprite.frame += 1 ## Increase the sprite animation frame by 1
+			walkTimer.start() ## Starts the timer for the delay, so it wont loop the animation super fast
 	velocity = move.normalized() * movementSpeed ## Creates a velocity by using the base velocity set at the start and the direction pressed
 	##Using the .normalized function allows the player to not move much faster in a diagonal than other directions
 	move_and_slide()##Tells Godot the character wants to move
